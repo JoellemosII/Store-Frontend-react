80e121a0a0ae6c7dcc4ffa9310fe75aa234f803b
joel <joel94master@gmail.com>
2024-11-15 20:44:52 -0300

    proyecto final

--- src/components/common/Cardd/Card.jsx
+++ src/components/common/Cardd/Card.jsx
@@ -1,15 +1,16 @@
 import "../Cardd/cardstyle.css";
 import { Link } from "react-router-dom";
-export const Card = ({ title, price, stock, image, id}) => {
+export const Card = ({ title, price, image, id}) => {
     return (
     <div className="card">
+        <h2>{title}</h2>
         <img src={image} alt="" />
-        <h2>Titulo: {title}</h2>
-        <h3>Precio: {price}</h3>
-        <h3>Stock: {stock}</h3>
+        <div className="propierity">
+        <p> ${price}</p>
         <Link to ={`/itemDetail/${id}`}>
-        <button>Ver detalle</button>
+        <button className="detalle">Ver detalle</button>
         </Link>
+        </div>
     </div>
     );
 };
