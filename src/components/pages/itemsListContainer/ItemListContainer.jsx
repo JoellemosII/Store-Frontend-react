48e7006b91f90c187708e1ec036f524938368917
joel <joel94master@gmail.com>
2024-11-14 21:46:13 -0300

    proyecto final

--- src/components/pages/itemsListContainer/ItemListContainer.jsx
+++ src/components/pages/itemsListContainer/ItemListContainer.jsx
@@ -30,12 +30,13 @@ export const ItemListContainer = () => {
     if (items.length === 0) {
         return (
         <>
-            <div className="loading">
+            <div className="loading">s
                 <Skeleton variant="rectangular" width={200} height={100} />
                 <Skeleton variant="text" sx={{fontSize:"1rem"}} width={200} height={50} />
                 <Skeleton variant="text" sx={{}}width={200} height={100} />
                 <Skeleton variant="text" sx={{}} width={200} height={100} />
             </div>
+            
         </>
         );
     }
